cbb19d09c2ad59c35282868ea8462254f8b65876
stopachka <stepan.p@gmail.com>
2016-10-21 18:25:00 +0200

    explore data hw

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-""" 
+"""
     Starter code for exploring the Enron dataset (emails + finances);
     loads up the dataset (pickled dict of dicts).
 
@@ -12,11 +12,57 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
+
 """
 
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+def data_points():
+    return len(enron_data)
 
+def num_features():
+    return len(enron_data.itervalues().next())
+
+def num_poi():
+    return len(filter(lambda p : p['poi'] == 1, enron_data.itervalues()))
+
+def stock_by_james():
+    return enron_data['PRENTICE JAMES']['total_stock_value']
+
+def colwell_messages():
+    return enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+
+def skilling_stock_options():
+    return enron_data['SKILLING JEFFREY K']['exercised_stock_options']
+
+def most_money():
+    def payments(k):
+        return [k, enron_data[k]['total_payments']]
+
+    return [
+        payments('SKILLING JEFFREY K'),
+        payments('LAY KENNETH L'),
+        payments('FASTOW ANDREW S')
+    ]
+
+def len_with(f):
+    return len(
+        filter(
+            f,
+            enron_data.itervalues()
+        )
+    )
+
+def percent_nan():
+    nan_len = len_with(lambda p : p['total_payments'] == 'NaN')
+    total = len(enron_data.itervalues())
+    return nan_len / total
+
+def percent_poi_nan():
+    vs = enron_data.itervalues()
+    pois = filter(lambda p : p['poi'] == 1, vs)
+    pois_nan = filter(lambda p : p['total_payments'] == 'NaN', pois)
+
+    return len(pois_nan) / len(pois)
