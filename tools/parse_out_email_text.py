0935207f56e3580dd2219d94f9610181e4679fb1
stopachka <stepan.p@gmail.com>
2016-10-22 17:55:33 +0200

    stem

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -3,17 +3,24 @@
 from nltk.stem.snowball import SnowballStemmer
 import string
 
+stemmer = SnowballStemmer('english')
+
+def stem(txt):
+    return ' '.join(
+        map(lambda word: stemmer.stem(word), txt.split(' '))
+    )
+
 def parseOutText(f):
     """ given an opened email file f, parse out all text below the
         metadata block at the top
         (in Part 2, you will also add stemming capabilities)
         and return a string that contains all the words
-        in the email (space-separated) 
-        
+        in the email (space-separated)
+
         example use case:
         f = open("email_file_name.txt", "r")
         text = parseOutText(f)
-        
+
         """
 
 
@@ -28,19 +35,20 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+
+        words = stem(text_string)
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
+
 
 
 
 
     return words
 
-    
+
 
 def main():
     ff = open("../text_learning/test_email.txt", "r")
@@ -51,4 +59,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
